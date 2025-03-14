0051c4900d5f5b4229bd286770ce3e5ce8781073
Benjamin S. Listyg <benjamin.listyg@ben.local>
2025-03-14 15:16:44 -0400

    updating CRAN imports

--- R/imports.R
+++ R/imports.R
@@ -1,6 +1,6 @@
 #' @importFrom utils write.csv
 #' @importFrom stats reorder
-#' @importFrom ggplot2 ggplot aes geom_bar labs scale_fill_brewer scale_fill_manual theme_minimal element_text facet_wrap coord_flip ggsave
+#' @importFrom ggplot2 ggplot aes geom_bar labs scale_fill_brewer scale_fill_manual theme_minimal element_text facet_wrap coord_flip ggsave theme
 #' @importFrom tibble tibble
 #' @importFrom dplyr bind_rows
 #' @importFrom purrr map_df
